fe56b2eb699d7a09b32406fc9a81597952194761
Ajay Rajput <ajayrajpoot.cs@gmail.com>
2024-01-02 13:33:58 +0530

    JAVA BASED CONFIG

---
+++ src/main/java/hibernate/client/App.java
@@ -0,0 +1,54 @@
+package hibernate.client;
+
+import org.hibernate.SessionFactory;
+
+import hibernate.configDB.HibernateConfig;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.cfg.Configuration;
+import hibernate.entity.Employee;
+
+public class App {
+	// @SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+
+		Employee e = new Employee();
+//		e.setId(2);
+		e.setName("Black Widow");
+		e.setGender("Male");
+		e.setSalary(200000);
+	
+
+		// Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+		// SessionFactory sf = cfg.buildSessionFactory();
+		// StandardServiceRegistry ssr = new
+		// StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		// Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		// SessionFactory sf = meta.buildSessionFactory();
+		
+
+		HibernateConfig cfg = new HibernateConfig();
+
+		SessionFactory sf = cfg.getSessionFactory();
+
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+
+		session.save(e);
+		// session.update(e)
+		// session.delete(e.getName(), e);;;
+		tx.commit();
+
+		// e = session.get(Employee.class, 4);
+		// System.out.println(e);
+
+		System.out.print("Suceess...");
+		session.close();
+
+	}
+
+}
---
+++ src/main/java/hibernate/configDB/HibernateConfig.java
@@ -0,0 +1,38 @@
+package hibernate.configDB;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+public class HibernateConfig {
+	
+	public SessionFactory getSessionFactory() {
+		
+		Properties ps = new Properties();
+		ps.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		ps.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernatedb");
+		ps.put(Environment.USER, "root");
+		ps.put("hibernate.connection.password", "root");
+		ps.put("hibernate.hbm2ddl.auto", "update");
+		ps.put(Environment.SHOW_SQL, "true");
+		ps.put(Environment.FORMAT_SQL, "true");
+		
+		StandardServiceRegistry ssr= new StandardServiceRegistryBuilder().applySettings(ps).build();
+		
+//		MetadataSources metasources = new MetadataSources(ssr).addAnnotatedClass(hibernate.entity.Employee.class);
+		//MetaData meta = metasources.buildMetadata();
+		//Direct Call
+		Metadata meta = new MetadataSources(ssr).addAnnotatedClass(hibernate.entity.Employee.class).buildMetadata();
+		
+		SessionFactory sf = meta.buildSessionFactory();
+		
+		return sf;
+		
+	}
+
+}
